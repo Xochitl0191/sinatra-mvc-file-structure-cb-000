10cefc9e5aaa0e811f5ee871457b3c00becca575
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-25 17:37:12 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+def initialize(name)
+  @name = name
+end
+
+ def name=(name)
+   @name = name
+ end
+
+def name
+  @name
+end
+
+end
+
+#name, breed, and age attributes which can be set on initialization.
