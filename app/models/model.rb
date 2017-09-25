8426822fb62aceb24b7fb2a2ac2f4a6d956129d3
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-25 17:28:01 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,19 +1,15 @@
 class Dog
-attr_accessor :name, :breed, :age
-DOGS = []
-def initialize(name, breed, age)
-  @name = name
-  @breed = breed
-  @age = age 
-  DOGS << self 
-end
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self 
+  end
 
- def name=(name)
-   @name = name
- end
+   def self.all? 
+     DOGS
+  end 
 
-def name
-  @name
-end
-
-end
+end 
